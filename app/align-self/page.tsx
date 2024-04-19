66a2e101635b17804a3a133f879b465ace124f30
ChWasam <wasam.developer@gmail.com>
2024-04-19 18:32:55 +0500

    updaed image

--- app/align-self/page.tsx
+++ app/align-self/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex justify-center items-center bg-white h-screen w-screen">
-      <div className=" flex flex-col items-end gap-y-6 w-[400px] bg-slate-50 rounded-lg   ">
+      <div className=" flex flex-col items-end gap-y-6 w-[400px] bg-slate-50 rounded-lg shadow-2xl   ">
         <Image
           className="m-auto h-[400px] w-[400px]"
           src={
-            "https://images.pexels.com/photos/21758774/pexels-photo-21758774/free-photo-of-hot-air-balloons-flying-over-the-rock-formations.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            "https://images.pexels.com/photos/17948350/pexels-photo-17948350/free-photo-of-woman-with-jewellery-on-her-face.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           }
           alt=""
           width={1000}
@@ -21,15 +21,17 @@ export default function Home() {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta cum
           impedit veniam Add to Cart
         </p>
-        <button className=" bg-teal-700 p-3 self-start  rounded-md ">Add to cart </button>
+        <button className=" bg-teal-700 p-3 self-start  rounded-md ">
+          Add to cart{" "}
+        </button>
       </div>
     </main>
   );
 }
 
 // Check button
-// look for items-end in top div 
-// look for    self-end    in button        
+// look for items-end in top div
+// look for    self-end    in button
 
-// flex will do changes in all the divs 
+// flex will do changes in all the divs
 // if want to so something different in one particular div then use self for that particular div
