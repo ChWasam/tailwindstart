40741a619823855bb247ad81159091e089355e80
ChWasam <wasam.developer@gmail.com>
2024-04-30 19:25:49 +0500

    Few comments  added

--- app/grid-flow-col/page.tsx
+++ app/grid-flow-col/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <main className="grid grid-rows-[auto,auto] grid-flow-col  m-20 justify-between">
+    <main className="grid grid-rows-[auto,auto] grid-flow-col   m-20 justify-between">
       {/*  matlab pehla pehla col fill ho ga phir dosra  */}
       <span>11.5k</span>
       <p>Tweets</p>
