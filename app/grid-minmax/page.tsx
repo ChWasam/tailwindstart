86409b0226532564f0c3ce0dfcae532a6126a1af
ChWasam <wasam.developer@gmail.com>
2024-04-30 13:39:37 +0500

    minmax in grid grid-cols-[repeat(3,minmax(auto,18rem))]

--- app/grid-minmax/page.tsx
+++ app/grid-minmax/page.tsx
@@ -1,15 +1,31 @@
+import React from "react";
 
+function page() {
+  return (
+    <main className="grid bg-red-100 text-white h-screen w-screen grid-cols-[minmax(auto,18rem)] md:grid-cols-[repeat(3,minmax(auto,18rem))]  justify-center content-center gap-10">
+      <div className="flex flex-col  bg-red-500 rounded-lg p-6  ">
+        <h1 className="text-4xl">Standard</h1>
+        <p>Monthly Plan</p>
+        <span>$25</span>
+      </div>
+      <div className="flex flex-col bg-red-500 rounded-lg p-6">
+        <h1 className="text-4xl">Standard</h1>
+        <p>Monthly Plan</p>
+        <span>$25</span>
+      </div>
+      <div className="flex flex-col bg-red-500  rounded-lg p-6 ">
+        <h1 className="text-4xl">Standard</h1>
+        <p>Monthly Plan</p>
+        <span>$25</span>
+      </div>
+    </main>
+  );
+}
 
+export default page;
 
-
-
-
-
-
-
-
-
-
-
-{/* 1fr = min width=(auto) maxwidth=1fr (jitni available space ho ge wo la lai ga ) */}
-// Difference between 0 and auto 
+{
+  /*  1fr (jitni available space ho ge wo la lai ga ) */
+}
+// md:grid-cols-[repeat(3,minmax(auto,18rem))] 
+// bydefault grid-cols-1 ka matlab hota ha md:grid-cols-[repeat(3,minmax(0,1fr))] 
