b54f04b619a3e37f169e449c294ffdfa5f560b8b
ChWasam <wasam.developer@gmail.com>
2024-04-30 18:48:46 +0500

    grid autofill and auto fill not done yet

---
+++ app/grid-autofitandautofill/page.tsx
@@ -0,0 +1,198 @@
+import Image from "next/image";
+import React from "react";
+
+function page() {
+  return (
+    <main className="grid grid-cols-[repeat(autofit,minmax(16rem,1fr))]  bg-slate-200 h-screen w-screen p-10 gap-20  ">
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40 "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40 "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40  "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-80 rounded-lg shadow-lg">
+        <Image
+          className=" rounded-t-lg  h-40 "
+          src={
+            "https://images.pexels.com/photos/22705119/pexels-photo-22705119/free-photo-of-black-man-b-w-photo.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          }
+          alt="picture Random"
+          width={1000}
+          height={1000}
+        />
+
+        <div className="flex flex-col rounded-b-lg  text-left p-4 bg-white gap-y-3">
+          <h1 className="font-extrabold text-3xl">Blog title 1</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            assumenda porro inventore repellendus ipsum.
+          </p>
+          <span className="text-purple-800">Read more</span>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default page;
