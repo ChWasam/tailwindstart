55ede695b46ecb1c37a1d0dc4b0e81dbc4a5be4e
ChWasam <wasam.developer@gmail.com>
2024-04-30 19:32:22 +0500

    Grid summary upgrade

--- app/grid-summary/page.tsx
+++ app/grid-summary/page.tsx
@@ -1,9 +1,19 @@
 /* justify-center content-center === aligns rows and
-column */ 
+column */
 // (used in parent div)
+
+import { Content } from "next/font/google"
 
 /* items-center justify-items-center ==== aligns content within container */
 // (also used in parent div)
 
 /* self-center justify-self-center ==== aligns content itself === grid items */
+
+// In case of 1fr =>
+// min width = according to content 
+// max width = remaining space 
+
+
+// in case of auto =>
+// width adjust according to Content 
 
